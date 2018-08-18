fbaad03321faab9752f449c848d2cbb8b2dd916a
Dmitry Vdovin <voidxnull@gmail.com>
2018-08-18 07:39:51 +0200

    Add iterators, refactor parse methods to not allocate

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,19 @@ mod kerning_value;
 mod page;
 mod rect;
 mod sections;
-mod string_parse_error;
 mod utils;
 
 pub use self::config_parse_error::ConfigParseError;
 pub use self::error::Error;
 pub use self::rect::Rect;
-pub use self::string_parse_error::StringParseError;
 
 use std::io::Read;
 use self::char::Char;
 use self::kerning_value::KerningValue;
 use self::page::Page;
 use self::sections::Sections;
+use std::str::Chars;
+use std::str::Lines;
 
 #[derive(Clone, Debug)]
 pub struct CharPosition {
@@ -94,91 +94,162 @@ impl BMFont {
         self.pages.as_slice()
     }
 
-    pub fn parse(&self, s: &str) -> Result<Vec<CharPosition>, StringParseError> {
-        let lines = try!(self.parse_lines(s));
-        let mut char_positions = Vec::new();
-        let mut y: i32 = 0;
-        for line in lines {
-            let mut x: i32 = 0;
-            let mut kerning_values: Vec<&KerningValue> = Vec::new();
-            for character in line {
-                let kerning_value = kerning_values.into_iter()
-                                                  .find(|k| k.second_char_id == character.id)
-                                                  .map(|k| k.value)
-                                                  .unwrap_or(0);
-                let page_rect = Rect {
-                    x: character.x as i32,
-                    y: character.y as i32,
-                    width: character.width,
-                    height: character.height,
-                };
-                let screen_x = x + character.xoffset + kerning_value;
-                let screen_y = match self.ordinate_orientation {
-                    OrdinateOrientation::BottomToTop => {
-                        y + self.base_height as i32 - character.yoffset - character.height as i32
-                    }
-                    OrdinateOrientation::TopToBottom => y + character.yoffset,
-                };
-                let screen_rect = Rect {
-                    x: screen_x,
-                    y: screen_y,
-                    width: character.width,
-                    height: character.height,
-                };
-                let char_position = CharPosition {
-                    page_rect: page_rect,
-                    screen_rect: screen_rect,
-                    page_index: character.page_index,
-                };
-                char_positions.push(char_position);
-                x += character.xadvance + kerning_value;
-                kerning_values = self.find_kerning_values(character.id);
-            }
-            match self.ordinate_orientation {
-                OrdinateOrientation::TopToBottom => y += self.line_height as i32,
-                OrdinateOrientation::BottomToTop => y -= self.line_height as i32,
-            }
-        }
-        Ok(char_positions)
+    pub fn char_positions<'str, 'font>(&'font self, string: &'str str) -> CharPositions<'str, 'font> {
+        CharPositions::new(string, self)
     }
 
     fn find_kerning_values(&self, first_char_id: u32) -> Vec<&KerningValue> {
         self.kerning_values.iter().filter(|k| k.first_char_id == first_char_id).collect()
     }
+}
 
-    fn parse_lines(&self, s: &str) -> Result<Vec<Vec<&Char>>, StringParseError> {
-        let mut lines = Vec::new();
-        let mut line = Vec::new();
-        let mut missing_characters = Vec::new();
-        let mut unsupported_characters = Vec::new();
-        for c in s.chars() {
-            if c == '\n' {
-                lines.push(line);
-                line = Vec::new();
-                continue;
-            }
-            if c.len_utf16() != 1 {
-                unsupported_characters.push(c);
-                continue;
-            }
-            let tmp_str = { let mut t = String::new(); t.push(c); t };
-            let char_id = tmp_str.encode_utf16().next().unwrap() as u32;
-            if let Some(c) = self.characters.iter().find(|c| c.id == char_id) {
-                line.push(c);
-            } else {
-                missing_characters.push(c);
-            }
-        }
-        if !line.is_empty() {
-            lines.push(line);
-        }
-        if missing_characters.is_empty() && unsupported_characters.is_empty() {
-            Ok(lines)
-        } else {
-            Err(StringParseError {
-                missing_characters: missing_characters,
-                unsupported_characters: unsupported_characters,
-            })
+pub struct TextLines<'str, 'font> {
+    all_chars: &'font [Char],
+    lines: Lines<'str>,
+}
+
+impl<'str, 'font> TextLines<'str, 'font> {
+    fn new(string: &'str str, all_chars: &'font [Char]) -> Self {
+        TextLines {
+            all_chars,
+            lines: string.lines(),
         }
     }
+}
+
+impl<'str, 'font> Iterator for TextLines<'str, 'font> {
+    type Item = TextLine<'str, 'font>;
+
+    fn next(&mut self) -> Option<TextLine<'str, 'font>> {
+        let substring = self.lines.next()?;
+        let line = TextLine::new(substring, self.all_chars);
+        Some(line)
+    }
+}
+
+pub struct TextLine<'str, 'font> {
+    all_chars: &'font [Char],
+    chars: Chars<'str>,
+}
+
+impl<'str, 'font> TextLine<'str, 'font> {
+    fn new(string: &'str str, all_chars: &'font [Char]) -> Self {
+        TextLine {
+            all_chars,
+            chars: string.chars(),
+        }
+    }
+}
+
+impl<'str, 'font> Iterator for TextLine<'str, 'font> {
+    type Item = Result<&'font Char, CharError>;
+
+    fn next(&mut self) -> Option<Result<&'font Char, CharError>> {
+        let c = self.chars.next()?;
+
+        if c.len_utf16() != 1 {
+            return Some(Err(CharError::UnsupportedCharacter(c)));
+        }
+
+        let char_id = c as u32;
+        if let Some(found_char) = self.all_chars.iter().find(|c| c.id == char_id) {
+            return Some(Ok(found_char));
+        } else {
+            return Some(Err(CharError::MissingCharacter(c)));
+        }
+    }
+}
+
+pub struct CharPositions<'str, 'font> {
+    font: &'font BMFont,
+    text_lines: TextLines<'str, 'font>,
+    text_line: TextLine<'str, 'font>,
+    x: i32,
+    y: i32,
+    prev_char_id: u32,
+}
+
+impl<'str, 'font> CharPositions<'str, 'font> {
+    fn new(string: &'str str, font: &'font BMFont) -> Self {
+        let mut text_lines = TextLines::new(string, &font.characters);
+        let text_line = text_lines.next().unwrap(); // FIXME
+
+        CharPositions {
+            font,
+            text_lines,
+            text_line,
+            x: 0,
+            y: 0,
+            prev_char_id: 0,
+        }
+    }
+}
+
+impl<'font, 'str> Iterator for CharPositions<'font, 'str> {
+    type Item = Result<CharPosition, CharError>;
+
+    fn next(&mut self) -> Option<Result<CharPosition, CharError>> {
+        let character = match self.text_line.next() {
+            Some(char) => match char {
+                Ok(char) => char,
+                Err(err) => return Some(Err(err)),
+            },
+            None => {
+                self.text_line = self.text_lines.next()?;
+                self.x = 0;
+
+                match self.font.ordinate_orientation {
+                    OrdinateOrientation::TopToBottom => self.y += self.font.line_height as i32,
+                    OrdinateOrientation::BottomToTop => self.y -= self.font.line_height as i32,
+                }
+
+                match self.text_line.next()? {
+                    Ok(char) => char,
+                    Err(err) => return Some(Err(err)),
+                }
+            },
+        };
+
+        let (x, y) = (self.x, self.y);
+
+        let kerning_value = self.font.kerning_values.iter()
+            .find(|k| k.first_char_id == self.prev_char_id && k.second_char_id == character.id)
+            .map(|k| k.value)
+            .unwrap_or(0);
+        let page_rect = Rect {
+            x: character.x as i32,
+            y: character.y as i32,
+            width: character.width,
+            height: character.height,
+        };
+        let screen_x = x + character.xoffset + kerning_value;
+        let screen_y = match self.font.ordinate_orientation {
+            OrdinateOrientation::BottomToTop => {
+                y + self.font.base_height as i32 - character.yoffset - character.height as i32
+            }
+            OrdinateOrientation::TopToBottom => y + character.yoffset,
+        };
+        let screen_rect = Rect {
+            x: screen_x,
+            y: screen_y,
+            width: character.width,
+            height: character.height,
+        };
+        let char_position = CharPosition {
+            page_rect,
+            screen_rect,
+            page_index: character.page_index,
+        };
+
+        self.x += character.xadvance + kerning_value;
+        self.prev_char_id = character.id;
+
+        Some(Ok(char_position))
+    }
+}
+
+#[derive(Debug)]
+pub enum CharError {
+    UnsupportedCharacter(char),
+    MissingCharacter(char),
 }
--- src/string_parse_error.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug)]
-pub struct StringParseError {
-    pub missing_characters: Vec<char>,
-    pub unsupported_characters: Vec<char>,
-}
